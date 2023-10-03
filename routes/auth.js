f7410dc92388a13ba4115de3c2e58688c4e0e9cb
Khrystyna Leshko <k.leshko@youngcapital.nl>
2023-10-03 16:50:40 +0200

    adding comments

--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,10 @@ const CryptoJS = require("crypto-js");
 // TO ASSIGN JWT TO PARTICULAR USERS / GIVE THEAM ACCESS
 const jwt = require("jsonwebtoken");
 
-
-
 // REGISTER
 
 router.post("/register", async (req, res) => {
+  // Create a new User instance with encrypted password
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
@@ -20,6 +19,7 @@ router.post("/register", async (req, res) => {
   });
 
   try {
+    // Save the new user to the database
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
   } catch (err) {
@@ -31,6 +31,7 @@ router.post("/register", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   try {
+    // Find a user by the provided username
     const user = await User.findOne(
       {
           username: req.body.username
@@ -38,7 +39,7 @@ router.post("/login", async (req, res) => {
     );
 
     if (!user) return res.status(401).json("Wrong Usernmame!");
-
+      // Decrypt the stored hashed password and compare with input password
     const hashedPassword = CryptoJS.AES.decrypt(
       user.password,
       process.env.PASS_SEC
@@ -50,7 +51,7 @@ router.post("/login", async (req, res) => {
   const inputPassword = req.body.password;
 
   if (originalPassword != inputPassword) return res.status(401).json("Wrong Password");
-
+  // Create a JWT for authentication
   const accessToken = jwt.sign(
     {
       id: user._id,
@@ -59,7 +60,7 @@ router.post("/login", async (req, res) => {
     process.env.JWT_SEC,
     {expiresIn: "3d"}
   );
-
+  // Return user data excluding the password and the access token
   const { password, ...others } = user._doc;
   res.status(200).json({...others, accessToken});
 
