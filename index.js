d6801e44d0a2697a2c31fdf6bbd8b042545d1a7e
Khrystyna Leshko <k.leshko@youngcapital.nl>
2023-10-03 15:28:20 +0200

    created all routes

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
 
 dotenv.config();
 
@@ -18,7 +22,10 @@ app.use(express.json());
 // ROUTES
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
-
+app.use("/api/product", productRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/order", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 
 app.listen(process.env.PORT || 5000, () => {
